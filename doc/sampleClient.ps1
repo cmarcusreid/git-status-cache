2e9f9781ab2fd78b8098efe44f27717608bcc183
Marcus Reid <marcus.reid@live.com>
2015-07-21 04:46:38 +0100

    Reuse pipe across script invocations sampleClient.

--- doc/sampleClient.ps1
+++ doc/sampleClient.ps1
@@ -1,8 +1,8 @@
-if ($pipe -eq $null)
+if ($Global:GitStatusCacheClientPipe -eq $null)
 {
-	$pipe = new-object System.IO.Pipes.NamedPipeClientStream '.','GitStatusCache','InOut','WriteThrough'
-	$pipe.Connect(50)
-	$pipe.ReadMode = 'Message'
+	$Global:GitStatusCacheClientPipe = new-object System.IO.Pipes.NamedPipeClientStream '.','GitStatusCache','InOut','WriteThrough'
+	$Global:GitStatusCacheClientPipe.Connect(50)
+	$Global:GitStatusCacheClientPipe.ReadMode = 'Message'
 }
 
 $timer = [system.diagnostics.stopwatch]::StartNew()
@@ -10,10 +10,10 @@ $timer = [system.diagnostics.stopwatch]::StartNew()
 $request = new-object psobject -property @{ Version = 1; Path = $PSScriptRoot } | ConvertTo-Json -Compress
 $encoding = [System.Text.Encoding]::Unicode
 $requestBuffer = $encoding.GetBytes($request)
-$pipe.Write($requestBuffer, 0, $requestBuffer.Length)
+$Global:GitStatusCacheClientPipe.Write($requestBuffer, 0, $requestBuffer.Length)
 
-$responseBuffer = new-object byte[] $pipe.InBufferSize
-$bytesRead = $pipe.Read($responseBuffer, 0, $responseBuffer.Length)
+$responseBuffer = new-object byte[] $Global:GitStatusCacheClientPipe.InBufferSize
+$bytesRead = $Global:GitStatusCacheClientPipe.Read($responseBuffer, 0, $responseBuffer.Length)
 $response = $encoding.GetString($responseBuffer, 0, $bytesRead)
 
 $timer.Stop()
